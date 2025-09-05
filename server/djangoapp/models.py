4b24ca375b2d5f5ed83e5eabdee895e80f4b71c8
Austin James <arjames1128@gmail.com>
2025-09-05 16:04:06 -0500

    View pylinter fixes

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -12,7 +12,7 @@ class CarMake(models.Model):
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake,
-                on_delete=models.CASCADE)  # Many-to-one relationship
+                                 on_delete=models.CASCADE)  # Many-to-one relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -21,14 +21,14 @@ class CarModel(models.Model):
         ('TRUCK', 'Truck'),
         ('SPORTS', 'Sports Car')
     ]
-    type = models.CharField(max_length=12, 
-            choices=CAR_TYPES, default='SUV') 
+    type = models.CharField(max_length=12,
+                            choices=CAR_TYPES, default='SUV') 
     year = models.IntegerField(default=2023,
     validators=[
         MaxValueValidator(2023),
         MinValueValidator(2015)
     ])
+    
     # Other fields as needed
     def __str__(self):
         return self.name  # Return the name as the string representation
-    
