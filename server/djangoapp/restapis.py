b308927972f9d747ff7a3b2303e208a292135b87
AustinRJames <arjames1128@gmail.com>
2025-09-05 14:37:20 -0400

    WIP for dynamic pages

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -16,26 +16,27 @@ def get_request(endpoint, **kwargs):
     if(kwargs):
         for key,value in kwargs.items():
             params=params+key+"="+value+"&"
-        
-        request_url  = backend_url+endpoint+"?"+params
-        print("GET from {} ".format(request_url))
-        try:
-            # Call get method of request library with URL and params
-            response = request.get(request_url)
-            return response.json()
-        except:
-            # If any error occurs
-            print("Network exception occured")
+
+    request_url = backend_url+endpoint+"?"+params
+
+    print("GET from {} ".format(request_url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(request_url)
+        return response.json()
+    except:
+        # If any error occurs
+        print("Network exception occurred")
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url+"analyze/"+text
     try:
         # Call get method of request lib with URL and params
-        response = request.get(request_url)
+        response = requests.get(request_url)
         return response.json()
     except Exception as err:
         print(f"Unexpected {err=}, {type(err)=}")
-        print("Netowrk exception occurred")
+        print("Network exception occurred")
 
 
 def post_review(data_dict):
